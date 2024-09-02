2315c521412b7d2f9081b9dbde9fc20ff9841c96
Your Name <you@example.com>
2024-09-02 17:10:45 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Credits
+[CSS](https://github.com/RemyIsCool/AnimatedFox)
+[homepage](https://github.com/Jaredk3nt/homepage)
---
+++ homepage/bookmarks.js
@@ -0,0 +1,38 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox"               , url: "https://app.tuta.com" },
+      { name: "GitHub"              , url: "https://github.com" },
+      { name: "Youtube"             , url: "https://youtube.com" },
+      { name: "Translate"           , url: "https://translate.google.com/?sl=auto&tl=vi&op=translate" },
+    ],
+  },
+  {
+    title: "AI",
+    links: [
+      { name: "ChatGPT"             , url: "https://chatgpt.com" },
+      { name: "Gemini"              , url: "https://gemini.google.com" },
+      { name: "BingChat"            , url: "https://www.bing.com/chat", },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/C_Programming"    , url: "https://reddit.com/r/C_Programming" },
+      { name: "/r/pcmasterrace"     , url: "https://reddit.com/r/pcmasterrace", },
+      { name: "/r/hacking"          , url: "https://reddit.com/r/hacking" },
+      { name: "/r/archlinux"        , url: "https://reddit.com/r/archlinux", },
+      { name: "/r/unixporn"         , url: "https://reddit.com/r/unixporn" },
+      { name: "/r/news"             , url: "https://reddit.com/r/news" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Zalo"                , url: "https://chat.zalo.me" },
+      { name: "Facebook"            , url: "https://facebook.com" },
+    ],
+  },
+];
